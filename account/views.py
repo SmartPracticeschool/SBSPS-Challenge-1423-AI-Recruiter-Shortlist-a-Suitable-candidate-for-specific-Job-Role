8c20179d4ba60fe773574c17f84dd87d6eeb9314
Karmakar Sudip S <49753535+VeNOM4171@users.noreply.github.com>
2020-07-28 13:00:46 +0530

    Update views.py

--- account/views.py
+++ account/views.py
@@ -41,7 +41,7 @@ def Insertrecord(request):
         college_name=data['college_name']
         skills=data['skills']
         ref=random.randint(0,22233)
-        user=UserResumes(Name=Name,email=email,mobile=mobile,company=company,experience=experience,experience_in_year=experience_in_year,designation=designation,college_name=college_name,skills=skills,reference=ref)
+        user=UserResumes(Name=Name,email=email,mobile=mobile,company=company,experience=experience,experience_in_year=experience_in_year,designation=designation,college_name=college_name,skills=skills)
         user.save()
         
         
